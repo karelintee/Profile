d1c67a943e431bbaa43c92be3d0f3ec65001addd
karelintee <149984324+karelintee@users.noreply.github.com>
2026-09-12 23:11:38 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -21,6 +21,48 @@ const SUBJECTS = [
         "type": "PDF",
         "url": "https://drive.google.com/file/d/1-tCLsbP20ccAcPGPhmicxqk-xppXFKT-/view?usp=sharing",
         "meta": ""
+      },
+      {
+        "name": "Unit 1: Database Foundations",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/1bZJogqeFMVoC2sbzxGhngE4VUlWlHJlc/edit?usp=sharing&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
+      },
+      {
+        "name": "Unit 2: Components of a Database Environment",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/19FNwBZO0yEZQVNBhXErPwQ2HNjC_Tn_a/edit?usp=drive_link&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
+      },
+      {
+        "name": "Unit 3: Data Models and Entity Relationship Modeling",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/1bK5R9800jbbRT5Plg0K9GnJ6VYvdYAaA/edit?usp=drive_link&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
+      },
+      {
+        "name": "Unit 4: Keys and Relationships",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/1fKynJs8hO5vbMV2V1ur5SRYvq17T1mBS/edit?usp=drive_link&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
+      },
+      {
+        "name": "Unit 5: Relational Database Model and Dita Dictionary",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/1dRbwa3bHCDAnSkgP_1mCFhw9fQxR2Vvv/edit?usp=sharing&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
+      },
+      {
+        "name": "Unit 6: Functional Dependencies",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/1TSDW9BCDD_mo22dR83_3E0PLKa9w-UXX/edit?usp=drive_link&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
+      },
+      {
+        "name": "Unit 7: Database Normalization",
+        "type": "SLIDES",
+        "url": "https://docs.google.com/presentation/d/1Fy2o3Qzv9TuzM0vly8uTcRo7zEx_8xZx/edit?usp=drive_link&ouid=102404852901982301703&rtpof=true&sd=true",
+        "meta": ""
       }
     ]
   }
